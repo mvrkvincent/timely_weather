19d25d44d5ace1870cb6d243d2f0be571fd2ad71
Mark Vincent <mvrktopher@gmail.com>
2021-12-17 13:57:39 -0500

    Finalize

--- src/components/ForecastModule.js
+++ src/components/ForecastModule.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {CONDITION_PROPS} from '../utils/conditionProps';
 
 export const ForecastModule = ({day}) => {
-    const date = new Date(day.dt * 1000).toLocaleDateString();
+    const date = new Date(day.dt * 1000).toString().split(' ')[0];
     const weather = day.weather[0];
     const icon = CONDITION_PROPS[weather.icon].ico;
 
     return(
         <div className='ForecastModule'>
-            <span>{date}</span>
+            <span className='date'>{date}</span>
             <i class={`${icon}`} />
             <span>{Math.floor(day.temp.min)}°F / {Math.floor(day.temp.max)}°F</span>
         </div>
